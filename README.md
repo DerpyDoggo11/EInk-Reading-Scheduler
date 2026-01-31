2062ba5c035290cb067d1b1c006a28bd45f7cb74
DerpyDoggo11 <vrs18451@gmail.com>
2026-01-30 23:09:51 -0800

    sync

---
+++ README.md
@@ -0,0 +1,8 @@
+## PDF Annotator for E-Ink Displays
+
+An all in one solution for studying large textbooks:
+- Organizes text, images, and equations into a customizable format
+- Shows related terms / equations for key terms
+- Keeps track of progress throughout the textbook
+
+# A Hack Club Inkling submission
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Kindle GTK devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      target = "kindlehf";
+
+      cross = pkgs.pkgsCross.${{
+        kindlehf = "armv7l-hf-multiplatform";
+        kindlepw2 = "armv7l-multiplatform";
+      }.${target}};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.meson
+          pkgs.ninja
+          pkgs.pkg-config
+          pkgs.glib
+          pkgs.gtk2
+          cross.buildPackages.gcc
+          pkgs.vim
+          pkgs.tinyxxd
+        ];
+
+        shellHook = ''
+          echo "Kindle target: ${target}"
+          echo "Cross CC: ${cross.buildPackages.gcc}/bin/arm-linux-gnueabihf-gcc"
+        '';
+      };
+    };
+}
